6aecfa4b6c9f01bf676781ea0379817ec4c65cf4
Elonmark69 <elonmark69420@gmail.com>
2022-08-15 19:38:57 +0530

    Added stars

--- components/Home/globescript.js
+++ components/Home/globescript.js
@@ -2,19 +2,33 @@ import { useEffect, useState, Suspense } from 'react';
 import { Canvas, useLoader } from '@react-three/fiber';
 import EarthMap from '../../public/earthmodel.jpg';
 import { TextureLoader } from 'three';
-import { OrbitControls } from '@react-three/drei';
+import { OrbitControls, Stars } from '@react-three/drei';
 
 function Earth(props) {
   const colorMap = useLoader(TextureLoader, EarthMap.src);
   return (
     <>
       <ambientLight intensity={1} />
+      <Stars
+        radius={300}
+        factor={7}
+        fade={true}
+        saturation={0}
+        depth={60}
+        count={500}
+      />
       <mesh>
         <sphereGeometry args={[1, 32, 32]} />
         <meshPhongMaterial color="red" />
         <meshStandardMaterial map={colorMap} />
-        <OrbitControls enableRotate={true} enableZoom={true}
-        zoomSpeed={0.5} rotateSpeed={0.5} />
+        <OrbitControls
+          enableRotate={true}
+          enableZoom={true}
+          enablePan={true}
+          panSpeed={0.5}
+          zoomSpeed={0.5}
+          rotateSpeed={0.5}
+        />
       </mesh>
     </>
   );
