ed9d47feffdf873d307663cc5e81c9c407008327
Elonmark69 <elonmark69420@gmail.com>
2022-08-15 19:15:03 +0530

    3 files modified

--- components/Home/globescript.js
+++ components/Home/globescript.js
@@ -2,6 +2,7 @@ import { useEffect, useState, Suspense } from 'react';
 import { Canvas, useLoader } from '@react-three/fiber';
 import EarthMap from '../../public/earthmodel.jpg';
 import { TextureLoader } from 'three';
+import { OrbitControls } from '@react-three/drei';
 
 function Earth(props) {
   const colorMap = useLoader(TextureLoader, EarthMap.src);
@@ -12,6 +13,8 @@ function Earth(props) {
         <sphereGeometry args={[1, 32, 32]} />
         <meshPhongMaterial color="red" />
         <meshStandardMaterial map={colorMap} />
+        <OrbitControls enableRotate={true} enableZoom={true}
+        zoomSpeed={0.5} rotateSpeed={0.5} />
       </mesh>
     </>
   );
